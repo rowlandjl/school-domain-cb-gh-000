1ba6302cbbe88e733cae5a8eaccaa5ddadd96251
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-21 22:50:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+
+  def initialize(school_name)
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if !@roster[grade].nil?
+      @roster[grade] << student
+    else
+      @roster[grade] = [student]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+  end
+
+end
